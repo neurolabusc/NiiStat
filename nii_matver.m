e1902528dd91898910f79e119e020a3cd26b8bbc
Chris Rorden <rorden@mailbox.sc.edu>
2017-03-09 12:16:09 -0500

    Add version for mat files

---
+++ nii_matver.m
@@ -0,0 +1,9 @@
+function v = nii_matver
+%Returns latest preprocessing version
+%
+%Examples
+% v = nii_matver
+% fprintf('LIME version (YYYY.MMDD): %.4f\n', v.lime);
+
+v.lime = 2016.0802; %YYYY.MMDD
+
